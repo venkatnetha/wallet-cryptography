492b787062db7b058a2e4c7afa5bb2d1df0506a0
venkatnetha@gmail.com <venkatnetha@WINVK185043-7H4.corp.ncr.com>
2023-04-11 23:26:01 +0530

    after correcting the git issues

--- src/base58.rs
+++ src/base58.rs
@@ -1,9 +1,30 @@
-use hex;
+use primitive_types::U512;
 
-pub fn decode_string(input: &str) -> Vec<u8> {
-    return hex::decode(input).expect("decode-hex");
-}
+const BASE58_CHARS: [char; 58] = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
+    'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
+    'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+    'z',
+];
 
-pub fn encode_bytes(input: &[u8]) -> String {
-    return hex::encode(&input);
+pub fn convert_hex_to_base58(s: &str) -> String {
+    let b = U512::from_big_endian(&[58]);
+    let z = U512::from_big_endian(&[0]);
+
+    let mut n = U512::from_str_radix(s, 16).expect("parse-hex");
+    let mut r: Vec<u8> = vec![];
+
+    while n > z {
+        let rem = n.checked_rem(b).expect("remainder").byte(0);
+        r.push(rem);
+        n = n.checked_div(b).expect("division");
+    }
+
+    let mut rs = r
+        .into_iter()
+        .map(|b| BASE58_CHARS[b as usize])
+        .collect::<String>();
+    rs = rs.chars().rev().collect::<String>();
+
+    return rs;
 }
